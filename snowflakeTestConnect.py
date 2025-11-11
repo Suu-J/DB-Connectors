8b2bc7a2df307231799f496ff7133f5f2cc45a76
Suu J <sudhanshu.kmsh@gmail.com>
2025-11-11 18:18:49 +0530

    clarification for warehouse

--- snowflakeTestConnect.py
+++ snowflakeTestConnect.py
@@ -1,6 +1,6 @@
 import snowflake.connector
 
-# Set the connection parameters
+# emptied the connection params, but warehouse is mandatory to mention even if you have a default one for your account
 conn_params = {
     "account": "",
     "user": "",
@@ -10,19 +10,15 @@ conn_params = {
     "schema": "",
 }
 
-# Connect to Snowflake
 conn = snowflake.connector.connect(**conn_params)
 
-# Create a cursor
+# cur for cursor
 cur = conn.cursor()
 
-# Execute a query to get the row count from the table
 cur.execute("SELECT COUNT(*) FROM CONTENTWORKSPACE")
 
-# Fetch the row count
 row_count = cur.fetchone()[0]
 print("Row count:", row_count)
 
-# Close the cursor and the connection
 cur.close()
 conn.close()
